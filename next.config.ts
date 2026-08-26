b7b86a38f10e55ed7532e24a06de774ef25c72da
Jmdec <infinitech.justin2024@gmail.com>
2026-08-26 09:24:38 +0800

    fix frontend url

--- next.config.ts
+++ next.config.ts
@@ -26,7 +26,7 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "infinitech-api28.site", pathname: "/**" },
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "ui-avatars.com" },
-      { protocol: "https", hostname: "staging-alfima-two.vercel.app" },
+      { protocol: "https", hostname: "https://alfima-frontend.vercel.app/" },
     ],
   },
 
